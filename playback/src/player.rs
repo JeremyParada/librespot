f1aa823af24a60a68842760469dd0fe355572d2f
JeremyParada <jer.parada@duocuc.cl>
2026-09-14 01:02:34 -0300

    Apply rustfmt to the crossfade guard
    `cargo fmt` fails on the multi-line `let crossfade_ms` introduced when
    the duration clamp was added. Pure formatting, no behaviour change.

--- playback/src/player.rs
+++ playback/src/player.rs
@@ -1672,11 +1672,7 @@ impl Future for PlayerInternal {
 
             // Start overlapping with the next track once we are within the crossfade window of
             // the end. It has to be preloaded already, otherwise there is nothing to fade into.
-            let crossfade_ms = self
-                .config
-                .crossfade
-                .as_millis()
-                .min(u32::MAX as u128) as u32;
+            let crossfade_ms = self.config.crossfade.as_millis().min(u32::MAX as u128) as u32;
             if crossfade_ms > 0
                 && !passthrough
                 && self.crossfade.is_none()
